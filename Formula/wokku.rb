7c2bd23a50b7a3d14cb7181ba3f99999635770d1
johannesdwicahyo <csk.rage@gmail.com>
2026-04-08 17:12:30 +0700

    fix: add correct sha256 checksum

--- Formula/wokku.rb
+++ Formula/wokku.rb
@@ -3,7 +3,7 @@ class Wokku < Formula
   homepage "https://wokku.dev"
   url "https://raw.githubusercontent.com/johannesdwicahyo/wokku/main/cli/wokku"
   version "0.1.0"
-  sha256 :no_check
+  sha256 "6befad3151519c60f7d037a8b38b217ab8003f7c7ce0791b118a40996ec7f82d"
   license "MIT"
 
   depends_on "ruby"
