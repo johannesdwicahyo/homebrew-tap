3425624c6802a0c86408d10d0b904656e3a36fb0
johannesdwicahyo <csk.rage@gmail.com>
2026-04-25 16:52:05 +0700

    wokku 0.2.1: servers list + --server NAME

--- Formula/wokku.rb
+++ Formula/wokku.rb
@@ -2,8 +2,8 @@ class Wokku < Formula
   desc "CLI for Wokku - deploy and manage apps on Dokku servers"
   homepage "https://wokku.cloud"
   url "https://raw.githubusercontent.com/johannesdwicahyo/wokku/main/cli/wokku"
-  version "0.2.0"
-  sha256 "c7678cb6ad4c218a37d401ac5d1fb3784b046059c4ce663eb097902feb264800"
+  version "0.2.1"
+  sha256 "f256796b8ca17bc645212228e11395ef70f3d25b0d5445a5b31a74a8d5763bd5"
   license "MIT"
 
   depends_on "ruby"
