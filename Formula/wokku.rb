49088e3ff65f273b474c8cccd80f0f236ec5ab51
johannesdwicahyo <csk.rage@gmail.com>
2026-04-25 12:07:38 +0700

    wokku 0.1.2: fix default API URL to wokku.cloud

--- Formula/wokku.rb
+++ Formula/wokku.rb
@@ -2,8 +2,8 @@ class Wokku < Formula
   desc "CLI for Wokku - deploy and manage apps on Dokku servers"
   homepage "https://wokku.cloud"
   url "https://raw.githubusercontent.com/johannesdwicahyo/wokku/main/cli/wokku"
-  version "0.1.1"
-  sha256 "4fa9c037ea6a2e28b8508bd9a297c30408e85f1f98f4f89dc04970cc32604f61"
+  version "0.1.2"
+  sha256 "5b1ccc8e3fa149672e0c60ef6368674b8a64c15b4ed288eba700db2d459b59c0"
   license "MIT"
 
   depends_on "ruby"
