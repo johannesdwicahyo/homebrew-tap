81e6dc739a00b6b64801646c65d8b7b00725d546
johannesdwicahyo <csk.rage@gmail.com>
2026-04-26 05:23:53 +0700

    wokku 1.5.0: align formula version with CLI

--- Formula/wokku.rb
+++ Formula/wokku.rb
@@ -2,7 +2,7 @@ class Wokku < Formula
   desc "CLI for Wokku - deploy and manage apps on Dokku servers"
   homepage "https://wokku.cloud"
   url "https://raw.githubusercontent.com/johannesdwicahyo/wokku/main/cli/wokku"
-  version "0.6.0"
+  version "1.5.0"
   sha256 "f2422b434a38e0639541e16d2392b8a7c3bb8cb93ffc88eedcbab9050d0d0082"
   license "MIT"
 
