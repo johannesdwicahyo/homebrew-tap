acfe218a8f27ee8f43975862d8695627330e13e4
johannesdwicahyo <csk.rage@gmail.com>
2026-05-04 14:03:06 +0700

    wokku: bump sha256 — adds servers / multi-server resolver

--- Formula/wokku.rb
+++ Formula/wokku.rb
@@ -3,7 +3,7 @@ class Wokku < Formula
   homepage "https://wokku.cloud"
   url "https://wokku.cloud/cli/wokku.tar.gz"
   version "2.0.0"
-  sha256 "5463a34c17e326c0227d5fe0f45b65bbc02a61296b457643e48383abaa71ab55"
+  sha256 "ff220d9eaea42719f4329be395b98e9b2085e0b6ba9b9832debae8f038ce6820"
   license "MIT"
 
   depends_on "ruby"
